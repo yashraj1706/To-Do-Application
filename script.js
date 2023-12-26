102d29f13f4dfd04b9bd8893788c9cfcc566e197
Yash Raj Singh <126608859+yashraj1706@users.noreply.github.com>
2023-12-26 18:43:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,136 +1,61 @@
-const newTaskInput = document.querySelector("#new-task input");
-const tasksDiv = document.querySelector("#tasks");
-let deleteTasks, editTasks, tasks;
-let updateNote = "";
-let count;
+    let item,checkbox,lable,li,list,deleteButton;
+    list=document.querySelector(".list");
+    document.querySelector("#new-task").addEventListener("click",(e)=>{
+        e.preventDefault();
+        item=document.querySelector("#task").value;
+        document.querySelector("#task").value='';
+        if(item===''){
+            
+            alert("Enter a task first")
+        }
+        else{
+            li=document.createElement('li');
+            li.innerHTML=item;
+            li.setAttribute("class"," ");
+            img=document.createElement("img");
+            img.setAttribute('src','./imgs/trash-can-with-cover-svgrepo-com (1).png')
+            img.setAttribute('class','remove');
+            li.appendChild(img);
+            list.appendChild(li);
+            saveData()
+        }
+    })
 
+    let input=document.querySelector('input')
+    input.addEventListener('keypress',(e)=>{
+        if(e.key==="Enter"){
+            document.getElementById('new-task').click();
+        }
+    })
 
-window.onload = () => {
-  updateNote = "";
-  count = Object.keys(localStorage).length;
-  displayTasks();
-};
+    let checkUncheck;
+    document.querySelector('ul').addEventListener("click",(e)=>{
+        
+        if(e.target.tagName!=='UL'){
+            if(e.target.className==='checked'){
+            e.target.setAttribute('class',' ');
+            saveData()
+            return;
+            }
+            else if(e.target.className==='remove'){
+            
+            if(e.target.parentElement.tagName!=='UL'){
+                e.target.parentElement.remove();
+                saveData()
+            }
+        }
+        else{
+            e.target.setAttribute('class','checked');
+            saveData()
+        }
+        }
+        
+    })
 
-
-const displayTasks = () => {
-  if (Object.keys(localStorage).length > 0) {
-    tasksDiv.style.display = "inline-block";
-  } else {
-    tasksDiv.style.display = "none";
-  }
-
- 
-  tasksDiv.innerHTML = "";
-
-  
-  let tasks = Object.keys(localStorage);
-  tasks = tasks.sort();
-
-  for (let key of tasks) {
-    let classValue = "";
-
-    let value = localStorage.getItem(key);
-    let taskInnerDiv = document.createElement("div");
-    taskInnerDiv.classList.add("task");
-    taskInnerDiv.setAttribute("id", key);
-    taskInnerDiv.innerHTML = `<span id="taskname">${key.split("_")[1]}</span>`;
-    
-    let editButton = document.createElement("button");
-    editButton.classList.add("edit");
-    editButton.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
-    if (!JSON.parse(value)) {
-      editButton.style.visibility = "visible";
-    } else {
-      editButton.style.visibility = "hidden";
-      taskInnerDiv.classList.add("completed");
+    function saveData(){
+        localStorage.setItem("data",list.innerHTML);
     }
-    taskInnerDiv.appendChild(editButton);
-    taskInnerDiv.innerHTML += `<button class="delete"><i class="fa-solid fa-trash"></i></button>`;
-    tasksDiv.appendChild(taskInnerDiv);
-  }
-
- 
-  tasks = document.querySelectorAll(".task");
-  tasks.forEach((element, index) => {
-    element.onclick = () => {
-      
-      if (element.classList.contains("completed")) {
-        updateStorage(element.id.split("_")[0], element.innerText, false);
-      } else {
-        updateStorage(element.id.split("_")[0], element.innerText, true);
-      }
-    };
-  });
-
-
-  editTasks = document.getElementsByClassName("edit");
-  Array.from(editTasks).forEach((element, index) => {
-    element.addEventListener("click", (e) => {
-      
-      e.stopPropagation();
-    
-      disableButtons(true);
-     
-      let parent = element.parentElement;
-      newTaskInput.value = parent.querySelector("#taskname").innerText;
-      //set updateNote to the task that is being edited
-      updateNote = parent.id;
-      //remove task
-      parent.remove();
-    });
-  });
-
- 
-  deleteTasks = document.getElementsByClassName("delete");
-  Array.from(deleteTasks).forEach((element, index) => {
-    element.addEventListener("click", (e) => {
-      e.stopPropagation();
-      //Delete from local storage and remove div
-      let parent = element.parentElement;
-      removeTask(parent.id);
-      parent.remove();
-      count -= 1;
-    });
-  });
-};
-
-//Disable Edit Button
-const disableButtons = (bool) => {
-  let editButtons = document.getElementsByClassName("edit");
-  Array.from(editButtons).forEach((element) => {
-    element.disabled = bool;
-  });
-};
-
-
-const removeTask = (taskValue) => {
-  localStorage.removeItem(taskValue);
-  displayTasks();
-};
-
-
-const updateStorage = (index, taskValue, completed) => {
-  localStorage.setItem(`${index}_${taskValue}`, completed);
-  displayTasks();
-};
-
-
-document.querySelector("#push").addEventListener("click", () => {
-
-  disableButtons(false);
-  if (newTaskInput.value.length == 0) {
-    alert("Please Enter A Task");
-  } else {
-   
-    if (updateNote == "") {
-      updateStorage(count, newTaskInput.value, false);
-    } else {
-      let existingCount = updateNote.split("_")[0];
-      removeTask(updateNote);
-      updateStorage(existingCount, newTaskInput.value, false);
-      updateNote = "";
+    function restoreData(){
+        list.innerHTML=localStorage.getItem('data');
     }
-    count += 1;
-    newTaskInput.value = "";
-  }
-});
+    restoreData();
